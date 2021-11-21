4276572ac0282ec0421b967417d7778eb7acfbc8
Austin Bell <snarkeet@gmail.com>
2021-11-21 01:36:52 -0500

    added navbar animation

--- app.js
+++ app.js
@@ -22,3 +22,25 @@
 //         }
 //     }
 // })
+
+const navSlide = () => {
+    const burger = document.getElementById("burger");
+    const nav = document.getElementById("navLinks");
+    const navLinks = document.querySelectorAll("#navLinks li");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation =""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        })
+    })
+
+
+}
+
+navSlide();
