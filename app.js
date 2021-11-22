0a02bcd5ae078c27cb1d067dbb86b4650debaf03
Austin Bell <snarkeet@gmail.com>
2021-11-21 21:58:29 -0500

    added burger animation

--- app.js
+++ app.js
@@ -33,11 +33,13 @@ const navSlide = () => {
 
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation =""
+                link.style.animation = "";
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         })
+
+        burger.classList.toggle("toggle");
     })
 
 
