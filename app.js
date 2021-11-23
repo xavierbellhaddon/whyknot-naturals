25845a054588e06546bb238dfec9c602d212be34
Austin Bell <snarkeet@gmail.com>
2021-11-22 23:53:14 -0500

    added top bar and changed navLinksMin opacity

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// $(function () {
-//     $(document).scroll(function () {
-//         var $nav = $("#mainNavbar");
-//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-//     });
-// });
-
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     autoWidth: true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:5
-//         }
-//     }
-// })
-
 const navSlide = () => {
   const burger = document.getElementById("burger");
   // const nav = document.getElementById("navLinks");
